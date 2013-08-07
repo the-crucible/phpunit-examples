90c8b01157d720ec1df1ad9f908510c2b38de77a
Tejaswi Sharma <tejaswi.sharma@meritnation.com>
2013-08-07 11:18:38 +0530

    Added comments on 01 and 02 scripts

--- 01StackTest.php
+++ 01StackTest.php
@@ -1,18 +1,26 @@
 <?php
 /**
- * This class is my first class I am starting my journey of
- * unit tests with.
+ * This class is to just give you a glimpse that how phpunit works.
+ * For writing any test you need to so following things
+ * 
+ * 1. Write a class which ends with 'Test' and should 
+ *    extends PHPUnit_Framework_TestCase class
+ * 2. After that write and test functions which should start with 'test'
+ * 3. A test class could have multiple test functions
+ * 5. You can write other supporting functions also to be 
+ *    used in test functions but don't start their names with 'test'. 
+ *    Otherwise they will be considered as test functions. 
  */
 
 class StackTest extends PHPUnit_Framework_TestCase
 {
     /**
      * This method will be used to test
-     * array_pop method in php, Its a very
-     * simple test and lots of assertEquals
-     * method are used.
+     * count() function in php, Its a very
+     * simple test and multiple assertEquals
+     * method are used. Fire it by
      * 
-     * @test 
+     * $ ./bin/phpunit --colors 01StackTest
      */
     
     public function testPushAndPop()
@@ -21,6 +29,7 @@ class StackTest extends PHPUnit_Framework_TestCase
         $this->assertEquals(0, count($stack));
  
         array_push($stack, 'foo');
+        
         $this->assertEquals('foo', $stack[count($stack)-1]);
         $this->assertEquals(1, count($stack));
  
---
+++ 02DependencyTest.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * This class demonstrate the a lot of tests could be dependent on each others 
+ * and they could use the return value of each function executed as the input of another.
+ * 
+ * If any of the function failed to give the return value, if it get failed
+ * all the tests below will be skipped.
+ */
+
+class DependencyTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * This is a simple test but it returns a value also
+     * 
+     * @return array 
+     */
+    public function testEmpty()
+    {
+        $stack = array();
+        $this->assertEmpty($stack);
+ 
+        return $stack;
+    }
+ 
+    /**
+     * This method is also a test method but its execution depends upon
+     * the value returned by testEmpty method. If testEmpty method report
+     * any error this test will never be fired
+     * 
+     * @param array $stack This is an array return by testEmpty method
+     * @depends testEmpty 
+     */
+    public function testPush(array $stack)
+    {
+        array_push($stack, 'foo');
+        $this->assertEquals('foo', $stack[count($stack)-1]);
+        $this->assertNotEmpty($stack);
+ 
+        return $stack;
+    }
+ 
+    /**
+     * This method is also a test method but its execution depends upon
+     * the value returned by testEmpty method. If testEmpty method report
+     * any error this test will never be fired
+     * 
+     * @param array $stack This is an array return by testEmpty method
+     * @depends testPush
+     */
+    public function testPop(array $stack)
+    {
+        $this->assertEquals('foo', array_pop($stack));
+        $this->assertEmpty($stack);
+    }
+}
+?>
